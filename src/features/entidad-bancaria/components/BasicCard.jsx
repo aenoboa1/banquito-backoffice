3cfd1f76201b794e7c2b008b8c558f620348fa71
JeffNolasco <95046541+JeffNolasco@users.noreply.github.com>
2023-08-28 14:16:10 -0500

    Update BasicCard.jsx
    nuevos cambios jeff

--- src/features/entidad-bancaria/components/BasicCard.jsx
+++ src/features/entidad-bancaria/components/BasicCard.jsx
@@ -5,6 +5,9 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import BranchForm from "../../gestion-sucursales/components/BranchForm";
+import BranchesList from "../../gestion-sucursales/components/BranchesList";
+import BankEntityList from "./BankEntityList";
 
 const bull = (
 	<Box
@@ -17,28 +20,13 @@ const bull = (
 
 export default function BasicCard() {
 	return (
-		<Card sx={{ minWidth: 275 }}>
+		<Card>
 			<CardContent>
-				<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-					Word of the Day
+				<Typography variant="h5" gutterBottom>
+					Entidad Bancaria
 				</Typography>
-				<Typography variant="h5" component="div">
-					be{bull}nev{bull}o{bull}lent
-				</Typography>
-				<Typography sx={{ mb: 1.5 }} color="text.secondary">
-					adjective
-				</Typography>
-
-
-				<Typography variant="body2">
-					well meaning and kindly.
-					<br />
-					{'"a benevolent smile"'}
-				</Typography>
+				<BankEntityList></BankEntityList>
 			</CardContent>
-			<CardActions>
-				<Button size="small">Learn More</Button>
-			</CardActions>
 		</Card>
 	);
 }
